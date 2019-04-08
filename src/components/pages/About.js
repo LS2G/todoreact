329ef005fd6969dc6bfc3467ec854783268af4c0
Yahotep Pelagie <sandrinegambouguercy@gmail.com>
2019-04-08 17:16:23 +0200

    initial commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+function About() {
+	return ( 
+		<React.Fragment>
+		<h1>A Propos</h1>
+		<p>Ceci est l' application TodoList version 1.0.0</p>
+		</React.Fragment>
+
+		)
+}
+
+export default About;
